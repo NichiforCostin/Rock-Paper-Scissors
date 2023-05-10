f675c5eb0a158cdad167c77af1f9001bde74f2a6
Nichifor Costin <costin.nk@gmail.com>
2023-05-10 10:11:34 +0300

    Add code for playing a game of 5 rounds with the results being shown in the console.

--- script.js
+++ script.js
@@ -3,26 +3,56 @@ function getComputerChoice() {
     const words = ["rock", "paper", "scissors"];
     return words[Math.floor(Math.random() * words.length)];
 }
-const computerChoice = getComputerChoice()
-console.log(computerChoice);
+
 
 function getPlayerChoice() {
     return prompt("Please choose between (rock, paper and scissors) :");
 }
-const playerChoice = getPlayerChoice();
-console.log(playerChoice);
+
+
 
 function playRound(computerChoice, playerChoice) {
+
     if(computerChoice === playerChoice) {
-        return `It's a tie! You both picked ${playerChoice}.`;
+        console.log(`It's a tie! You both picked ${playerChoice}.`); 
+        return 0;
     } else if (computerChoice === "rock" && playerChoice === "paper") {
-        return "You win! Paper beats rock.";
+        console.log("You win! Paper beats rock.");
+        return 1;
     } else if (computerChoice === "paper" && playerChoice === "scissors") {
-        return "You win! Scissors beats paper.";
+        console.log("You win! Scissors beats paper.");
+        return 1;
     } else if (computerChoice === "scissors" && playerChoice === "rock") {
-        return "You win! Rock beats Scissors."
+        console.log("You win! Rock beats Scissors."); 
+        return 1;
     } else {
-        return `You lose! ${computerChoice} beats ${playerChoice}.`;
+        console.log(`You lose! ${computerChoice} beats ${playerChoice}.`);
+        return -1;
     }
 }
-console.log(playRound(computerChoice, playerChoice));
+let playerScore = 0;
+let computerScore = 0;
+let numberOfRounds = 5;
+let computerChoice;
+let playerChoice;
+let winner;
+function game () {
+    while(numberOfRounds != 0) {
+        computerChoice = getComputerChoice();
+        playerChoice = getPlayerChoice().toLowerCase();
+        winner = playRound(computerChoice, playerChoice)
+        if(winner == 1) {
+            playerScore++;
+            numberOfRounds--;
+        } else if(winner == -1) {
+            computerScore++;
+            numberOfRounds--;
+        }
+        
+    }
+    if(playerScore > computerScore)
+        console.log(`Player beats Computer with ${playerScore} to ${computerScore}`);
+    else
+        console.log(`Computer beats Player with  ${computerScore} to ${playerScore}`);
+}
+game();
