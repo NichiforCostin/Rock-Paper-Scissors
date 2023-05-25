d8b779a1f093790c41b1d00d00fb17d3b9bda5e7
Nichifor Costin <costin.nk@gmail.com>
2023-05-25 19:07:34 +0300

    Add variable for winner display and added function that plays one round of rock paper scissors and displays the winner.

--- script.js
+++ script.js
@@ -4,11 +4,31 @@ function getComputerChoice() {
     return options[randomIndex];
 }
 
+function playRound(playerPick, computerPick) {
+  if(playerPick === computerPick) {
+    winnerParagraph.textContent = "The winner of the round is: No one. It was a tie.";
+    
+  } else if (playerPick == "rock" && computerPick == "scissors") {
+    winnerParagraph.textContent = "The winner of the round is: Player.";
+    return 1;
+  } else if (playerPick == "paper" && computerPick == "rock") {
+    winnerParagraph.textContent = "The winner of the round is: Player.";
+    return 1;
+  } else if (playerPick == "scissors" && computerPick == "paper") {
+    winnerParagraph.textContent = "The winner of the round is: Player.";
+    return 1;
+  } else {
+    winnerParagraph.textContent = "The winner of the round is: Computer";
+    return -1;
+  }
+}
+
 const rockButton = document.getElementById("rockButton");
 const paperButton = document.getElementById("paperButton");
 const scissorsButton = document.getElementById("scissorsButton");
 const playerChoice = document.getElementById("playerChoice");
 const computerChoice = document.getElementById("computerChoice");
+const winnerParagraph = document.getElementById("winnerOfTheRound");
 
 let playerPick = ""; 
 
@@ -17,7 +37,7 @@ rockButton.addEventListener('click', function() {
   computerPick = getComputerChoice();
   playerChoice.textContent = `The player choice is: ${playerPick}`;
   computerChoice.textContent = `The computer choice is: ${computerPick}`;
-
+  playRound(playerPick, computerPick);
 
 });
 
@@ -26,6 +46,7 @@ paperButton.addEventListener('click', function() {
   computerPick = getComputerChoice();
   playerChoice.textContent = `The player choice is: ${playerPick}`;
   computerChoice.textContent = `The computer choice is: ${computerPick}`;
+  playRound(playerPick, computerPick);
   
 });
 
@@ -34,6 +55,7 @@ scissorsButton.addEventListener('click', function() {
   computerPick = getComputerChoice();
   playerChoice.textContent = `The player choice is: ${playerPick}`;
   computerChoice.textContent = `The computer choice is: ${computerPick}`;
+  playRound(playerPick, computerPick);
 
 });
 
